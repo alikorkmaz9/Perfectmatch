aea58b93639fe32b4de33c7177d07a57e9a64153
alikorkmaz9 <74213404+alikorkmaz9@users.noreply.github.com>
2021-02-24 22:37:21 +0300

    feat: implement splash screen

--- Perfectmatch/ViewController.swift
+++ Perfectmatch/ViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    func textType() {
+        let title = UILabel()
+        title.sizeToFit()
+        title.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 400)
+        title.textAlignment = .center
+        title.font = UIFont(name: "Marker Felt Thin", size: view.frame.width/8)
+        title.textColor = .green
+        title.text = "PerfectMatch"
+        title.numberOfLines = 1
+        self.view.addSubview(title)
+        title.center = self.view.center
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textType()
     }
 
 
